9b76cae02aa5aa44cb87b7da86518760f2113575
Cunningham <cunningm@mail.gvsu.edu>
2017-09-29 13:38:16 -0400

    Initial Commit.

---
+++ MyApplication/app/src/main/java/edu/gvsu/cis/hw4_geocalc/GeoCalcActivity.java
@@ -0,0 +1,13 @@
+package edu.gvsu.cis.hw4_geocalc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GeoCalcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geo_calc);
+    }
+}
